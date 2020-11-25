0563b88ad65dce0f4cc3c5f0858a2d1b7a2e3280
TaoFu-surface <liuyuwei@buaa.edu.cn>
2020-11-25 16:16:48 +0800

    P5

--- P5/CPU/mips.v
+++ P5/CPU/mips.v
@@ -35,6 +35,7 @@ module mips(
     .jump(jump),
 	.clk(clk),
 	.reset(reset),
+	
     .PC_4(PC_4_IF_to_ID),
 	.Instr(Instr_IF_to_ID),
 	.PC(PC_IF_to_ID)
@@ -56,6 +57,7 @@ module mips(
 	.RegWrite(RegWrite),
 	.WData(RegWriteData),
 	.WritePC(RegWritePC),
+	
     .Rs_ID_to_EX(ReadAddr0_ID_to_EX),
     .Rt_ID_to_EX(ReadAddr1_ID_to_EX),
     .RegWriteAddr_ID_to_EX(RegWriteAddr_ID_to_EX), //非指令中Rd，而是真实的需写入的GPR地址
@@ -70,5 +72,52 @@ module mips(
     .jump(jump),
     .branch_addr32(branch_addr32),
     .jump_addr32(jump_addr32)
+    );
+	 
+	//////////////////// EX /////////////////////////////////
+	wire [4:0] ReadAddr0_EX_to_Mem, ReadAddr1_EX_to_Mem, RegWriteAddr_EX_to_Mem;
+	wire [31:0] PC_EX_to_Mem, DMWriteData_EX_to_Mem, ALUOut_EX_to_Mem;
+	wire [59:0] InstrType_EX_to_Mem;
+	Ex(
+    .Rs_ID_to_EX(ReadAddr0_ID_to_EX),
+    .Rt_ID_to_EX(ReadAddr1_ID_to_EX),
+    .RegWriteAddr_ID_to_EX(RegWriteAddr_ID_to_EX), //非指令中Rd，而是真实的需写入的GPR地址
+    .Shamt_ID_to_EX(Shamt_ID_to_EX),
+    .imm32_ID_to_EX(imm32_ID_to_EX),
+	.InstrType_ID_to_EX(InstrType_ID_to_EX),
+	.RsData_ID_to_EX(Data0_ID_to_EX),
+	.RtData_ID_to_EX(Data1_ID_to_EX),
+    .luiRes_ID_to_EX(luiRes_ID_to_EX),
+	.PC_ID_to_EX(PC_ID_to_EX),
+	.clk(clk),
+    .reset(reset),
+	
+	.PC_EX_to_Mem(PC_EX_to_Mem),
+	.Rs_EX_to_Mem(ReadAddr0_EX_to_Mem),
+    .Rt_EX_to_Mem(ReadAddr1_EX_to_Mem),
+    .RegWriteAddr_EX_to_Mem(RegWriteAddr_EX_to_Mem),
+	.InstrType_EX_to_Mem(InstrType_EX_to_Mem),
+	.ALUOut_EX_to_Mem(ALUOut_EX_to_Mem),
+	.DMWriteData_EX_to_Mem(DMWriteData_EX_to_Mem) //待写入DM的数据
+    );
+	
+	////////////////////// Mem ////////////////////////////
+	wire [31:0] ALUOut_Mem_to_WB, DMRead_Mem_to_WB, PC_Mem_to_WB;
+	wire [4:0] RegWriteAddr_Mem_to_WB;
+	Mem Mem(
+	.Rs_EX_to_Mem(ReadAddr0_EX_to_Mem),
+    .Rt_EX_to_Mem(ReadAddr1_EX_to_Mem),
+    .RegWriteAddr_EX_to_Mem(RegWriteAddr_EX_to_Mem),
+	.InstrType_EX_to_Mem(InstrType_EX_to_Mem),
+	.ALUOut_EX_to_Mem(ALUOut_EX_to_Mem),
+	.DMWriteData_EX_to_Mem(DMWriteData_EX_to_Mem), //待写入DM的数据
+	.PC_EX_to_Mem(PC_EX_to_Mem),
+	.clk(clk),
+    .reset(reset),
+	
+	.ALUOut_Mem_to_WB(ALUOut_Mem_to_WB),
+	.DMRead_Mem_to_WB(DMRead_Mem_to_WB),
+	.RegWriteAddr_Mem_to_WB(RegWriteAddr_Mem_to_WB),
+	.PC_Mem_to_WB(PC_Mem_to_WB)
     );
 endmodule
