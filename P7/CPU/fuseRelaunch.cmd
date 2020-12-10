ba5fbf80a1b2cc682255a66a9b644334cef70341
TaoFu-0204 <liuyuwei@buaa.edu.cn>
2020-12-10 11:16:36 +0800

    P7

--- P7/CPU/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "F:/Course/CO/P6_new/CPU/CPU_TestBench_isim_beh.exe" -prj "F:/Course/CO/P6_new/CPU/CPU_TestBench_beh.prj" "work.CPU_TestBench" "work.glbl" -mt "off" 
