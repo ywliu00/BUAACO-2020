5277a275a1e2f61e6a66ce18122dc46632d57841
TaoFu-surface <liuyuwei@buaa.edu.cn>
2020-12-11 15:33:50 +0800

    P7

--- P7/CPU_P7/CP0_Bridge.v
+++ P7/CPU_P7/CP0_Bridge.v
@@ -21,6 +21,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 module CP0_Bridge(
     input wire [59:0] InstrType,
+	input wire [59:0] WBInstrType,
     input wire [31:0] PCAddr,
 	input wire [31:0] DataIn,
 	input wire Err,
@@ -29,14 +30,37 @@ module CP0_Bridge(
 	output wire ErrSignal,
 	output wire [31:0] DataOut,
 	// CPU交互
-	
+
+	input wire [31:0] SR_out,
+	input wire [31:0] EPC_out,
+	input wire [31:0] Cause_out,
 	output wire CP0WriteEn,
 	output wire [31:0] SR,
 	output wire [31:0] EPC,
 	output wire [31:0] Cause
 	// CP0交互
     );
+	wire [4:0] ExcCode;
+	wire [7:2] IP;
+	wire WBJump, WBBranch;
+	assign ErrSignal = Err | (| HWInt);
 	
+	assign ExcCode = Err ? ErrStat : 
+					 (| HWInt) ? 5'd0 : 5'd31;
+	assign IP = HWInt;
+	assign Cause = {16'd0, IP, 3'd0, ExcCode, 2'd0};
 	
+	assign WBJump = WBInstrType == `inst_j || 
+					WBInstrType == `inst_jal ||
+					WBInstrType == `inst_jr ||
+					WBInstrType == `inst_jalr ? 1 : 0;
+	assign WBBranch = WBInstrType == `inst_beq ||
+					  WBInstrType == `inst_bgez ||
+					  WBInstrType == `inst_bgtz ||
+					  WBInstrType == `inst_blez ||
+					  WBInstrType == `inst_bltz ||
+					  WBInstrType == `inst_bne ? 1 : 0;
+	assign EPC = (WBJump || WBBranch) ? PCAddr - 32'd4 : PCAddr;
+	// 是否在延迟槽内
 	
 endmodule
